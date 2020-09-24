f725d715b70f4e6dc1655953b09bc845402a2d12
unknown <draz@ualberta.ca>
2020-09-23 20:05:48 -0600

    server and database updates

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const favicon = require("serve-favicon");
 const logger = require("morgan");
-require("./config/database");
+require("./build/config/database");
 
 const app = express();
 
@@ -11,7 +11,13 @@ app.use(express.json());
 
 app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
 app.use(express.static(path.join(__dirname, "build")));
+
 app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.post("/*", function (req, res) {
+  console.log(req.body);
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
