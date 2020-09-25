99afef9facebeb6df0ee91b18f8f7c1f36df0582
unknown <draz@ualberta.ca>
2020-09-25 16:34:47 -0600

    name changes

--- src/components/API/API.jsx
+++ src/components/API/API.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { getWordId } from "../../services/API";
 
-export class Document extends Component {
+export class API extends Component {
   state = {
     id: "Welcome",
     definition: "Search for a word and find its definition",
